26bbf2dae5e1fe709e5e5d31a0aa00636996ffac
Alex Shakirov <alexshakirov74@gmail.com>
2021-06-07 20:15:16 +0500

    feat: updated buttons

--- frontend/projects/bookolog-ui-kit/src/lib/ui-button/directives/button.directive.spec.ts
+++ frontend/projects/bookolog-ui-kit/src/lib/ui-button/directives/button.directive.spec.ts
@@ -1,8 +1,25 @@
 import { ButtonDirective } from './button.directive';
 
 describe('ButtonDirective', () => {
+  let directive: ButtonDirective = null;
+
+  beforeEach(() => {
+    directive = new ButtonDirective(null);
+  });
+
   it('should create an instance', () => {
-    const directive = new ButtonDirective(null);
     expect(directive).toBeTruthy();
+  });
+
+  describe('ButtonDirective', () => {
+    it('should return true for HEX', () => {
+      const result = directive.isHex('#713FC2');
+      expect(result).toBeTrue();
+    });
+
+    it('should return false for color name', () => {
+      const result = directive.isHex('primary');
+      expect(result).toBeFalse();
+    });
   });
 });
--- frontend/projects/bookolog-ui-kit/src/lib/ui-button/directives/button.directive.ts
+++ frontend/projects/bookolog-ui-kit/src/lib/ui-button/directives/button.directive.ts
@@ -7,9 +7,33 @@ export class ButtonDirective implements OnInit {
   @Input()
   public color: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' | string = 'primary';
 
+  @Input()
+  public type: 'basic' | 'flat' | string = 'basic';
+
   constructor(private elRef: ElementRef<HTMLElement>) {}
 
   public ngOnInit(): void {
-    this.elRef.nativeElement.classList.add('btn', `btn-${this.color}`);
+    this.bindColor();
+    this.bindStyle();
+  }
+
+  public bindColor(): void {
+    if (this.isHex(this.color)) {
+      this.elRef.nativeElement.style.backgroundColor = this.color;
+    } else {
+      this.elRef.nativeElement.classList.add('btn', `btn-${this.color}`);
+    }
+  }
+
+  public bindStyle(): void {
+      this.elRef.nativeElement.classList.add(`btn-${this.type}`);
+  }
+
+  public isHex(color: string): boolean {
+    color = color.replace('#', '');
+
+    const num = parseInt(color, 16);
+
+    return Number.isFinite(num);
   }
 }
